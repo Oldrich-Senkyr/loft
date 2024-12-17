bc796b42e37b3bb6caf4a402820b562d086be2db
Oldrich Senkyr <senkyr@dknl.cz>
2024-12-17 22:09:22 +0100

    Changes for 2024-12-17 22:09

--- agent/admin.py
+++ agent/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Person
-admin.site.register(Person)
+from django.utils.translation import gettext_lazy as _
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['display_name', 'first_name', 'last_name', 'organization', 'get_role_display']
+    fieldsets = (
+        (None, {
+            'fields': ('display_name', 'first_name', 'last_name', 'organization', 'role', 'title_before', 'title_after')
+        }),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        # Použití překladu pro názvy polí přímo v Adminu
+        fieldsets = super().get_fieldsets(request, obj)
+        return fieldsets
+
+admin.site.register(Person, PersonAdmin)
 
