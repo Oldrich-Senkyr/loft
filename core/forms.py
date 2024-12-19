8d6b0bac1a7f8eb2707b30097b9be4756be48c8e
Oldrich Senkyr <senkyr@dknl.cz>
2024-12-19 15:37:32 +0100

    Changes for 2024-12-19 15:37

--- core/forms.py
+++ core/forms.py
@@ -1,43 +1,47 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.utils.translation import gettext_lazy as _
 
+# Dynamically get the custom user model
+User = get_user_model()
+
 class LoginForm(AuthenticationForm):
-    username  = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': _('Your username'),  # Překlad
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': _('Your username'),
         'class': 'w-full py-4 rounded-xl'
     }))
 
-    password  = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': _('Your password'),  # Překlad
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': _('Your password'),
         'class': 'w-full py-4 rounded-xl'
     }))
 
 
 class SignupForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ('username','email','password1','password2')
+        model = User  # Use the dynamically retrieved user model
+        fields = ('username', 'email', 'password1', 'password2')
     
-    username  = forms.CharField(
+    username = forms.CharField(
         label=_("Username"), 
         widget=forms.TextInput(attrs={
             'placeholder': _('Your username'),
             'class': 'w-full py-4 rounded-xl'
-    }))
+        })
+    )
 
-    email  = forms.CharField(widget=forms.EmailInput(attrs={
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
         'placeholder': _('Your email address'),
         'class': 'w-full py-4 rounded-xl'
     }))
 
-    password1  = forms.CharField(widget=forms.PasswordInput(attrs={
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': _('Your password'),
         'class': 'w-full py-4 rounded-xl'
     }))
 
-    password2  = forms.CharField(widget=forms.PasswordInput(attrs={
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': _('Repeat password'),
         'class': 'w-full py-4 rounded-xl'
     }))
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import logout
 from django.utils import translation
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.urls import resolve, reverse
 
 # Changes for forms
 from .forms import SignupForm 
@@ -11,6 +12,16 @@ from .forms import SignupForm
 
 def index(request):
     return render(request, 'core/index.html')   #Kde lezi soubor
+
+def switch_language(request, language_code):
+    # Aktivace zvoleného jazyka
+    translation.activate(language_code)
+
+    # Uložení vybraného jazyka do cookies
+    response = redirect(request.META.get('HTTP_REFERER', 'core:index'))  # Přesměrování na předchozí stránku nebo domovskou stránku jako fallback
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language_code)
+
+    return response
 
 
 def switch_language(request, language_code):
@@ -25,6 +36,64 @@ def switch_language(request, language_code):
     referer = request.META.get('HTTP_REFERER')
 
     return response
+
+from django.shortcuts import redirect
+from django.utils import translation
+from django.urls import resolve, reverse
+from django.conf import settings
+
+from django.shortcuts import redirect
+from django.utils import translation
+from django.conf import settings
+from django.urls import resolve, reverse
+from urllib.parse import urlparse
+
+from django.shortcuts import redirect
+from django.utils import translation
+from django.conf import settings
+from django.urls import resolve, reverse
+from urllib.parse import urlparse
+
+from django.utils import translation
+from django.conf import settings
+from django.shortcuts import redirect
+from urllib.parse import urlparse
+
+from django.utils import translation
+from django.conf import settings
+from django.shortcuts import redirect
+from urllib.parse import urlparse
+from django.urls import set_urlconf
+
+def switch_language(request, language_code):
+    # Aktivace nového jazyka
+    translation.activate(language_code)
+
+    # Uložení jazyka do cookies
+    response = redirect('/')  # Výchozí přesměrování
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language_code)
+
+    # Zpracování refereru
+    referer = request.META.get('HTTP_REFERER')
+    if referer:
+        parsed_referer = urlparse(referer)
+        referer_path = parsed_referer.path
+
+        # Detekce jazykového prefixu v URL
+        for lang, _ in settings.LANGUAGES:
+            if referer_path.startswith(f'/{lang}/'):
+                # Nahrazení prefixu novým jazykem
+                referer_path = referer_path.replace(f'/{lang}/', f'/{language_code}/', 1)
+                break
+        else:
+            # Pokud prefix chybí, přidáme ho
+            referer_path = f'/{language_code}{referer_path}'
+
+        # Přesměrování na opravenou URL
+        response = redirect(referer_path)
+
+    return response
+
 
 
 def swl(request):
